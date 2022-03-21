bcb7096fea63ab4ded8a79303105cb87426f30b5
Pei Ming <76734355+seahpm99@users.noreply.github.com>
2022-03-21 21:47:37 +0800

    Update make_deposit.py

--- microservices/make_deposit.py
+++ microservices/make_deposit.py
@@ -30,7 +30,7 @@ def make_deposit():
 
             # do the actual work
             # 2. Accept Deposit amount
-            result = process_deposit(deposit)
+            result = processDeposit(deposit)
             return jsonify(result), result["code"]
 
         except Exception as e:
@@ -51,24 +51,43 @@ def make_deposit():
         "message": "Invalid JSON input: " + str(request.get_data())
     }), 400
 
-def process_deposit(deposit):
+def processDeposit(deposit):
     # 3. Retrieve the amount trader has 
     # Invoke the user info microservice
     print('\n-----Invoking user info microservice-----')
-    user_info_URL += "/" + deposit["accID"]
-    user_info = invoke_http(user_info_URL, method='GET')
+    new_user_info_URL = user_info_URL + "/" + deposit["email"]
+    user_info = invoke_http(new_user_info_URL, method='GET')
     print('user_info:', user_info)
-    # jsonify need to return accid and trad_accid
     
+    update_deposit = jsonify({
+        "AccID" : user_info["accID"],
+        "Trade_AccID" : user_info["trade_AccID"],
+        "Amount" : deposit["amount"]
+    })
+
     # 5. Update amount deposited 
     # Invoke trading account microservice
     print('\n-----Invoking trading account microservice-----')
-    trading_acc_URL += "/" + deposit["accID"] + "/" + deposit["currency"]
-    deposit_result = invoke_http(trading_acc_URL, method='PUT',json=deposit)
+    new_trading_acc_URL = trading_acc_URL + "/" + deposit["accID"] + "/" + deposit["currency"]
+    deposit_result = invoke_http(new_trading_acc_URL, method='PUT',json= update_deposit)
     print('deposit_result:', deposit_result)
-    # jsonify need to return tradeaccbalance
+    
+    deposit_log = jsonify({
+        "AccID" : deposit_result["accID"],
+        "Trade_AccID" : deposit_result["trade_AccID"],
+        "trade_Acc_Balance" : deposit_result["trade_Acc_Balance"]
+    })
 
-    # Check the order result; if a failure, send it to the error microservice.
+    # 7. Sent amount deposited
+    # Invoke transaction log microservice
+    print('\n\n-----Publishing the (deposit info) message with routing_key=deposit.info-----') 
+    # transaction_log_URL += "/" + deposit["accID"]
+    # invoke_http(transaction_log_URL, method='POST',json=deposit_result)
+    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="deposit.info", 
+            body=message)
+    print("\nDeposit action performed and updated transaction log.\n")
+
+    # Check the deposit result; if a failure, send it to the error microservice.
     code = deposit_result["code"]
     message = json.dumps(deposit_result)
 
@@ -91,13 +110,12 @@ def process_deposit(deposit):
         # 9. Return error
         return {
             "code": 500,
-            "data": {"deposit_result": deposit_result},
+            "data": {"deposit_log": deposit_log},
             "message": "Deposit action failure sent for error handling."
         }
     else:
 
-
-        # 7. Notify trader
+        # 8. Notify trader
         # Invoke email notification microservice
         print('\n\n-----Publishing the (deposit info) message with routing_key=deposit.info-----') 
         # invoke_http(email_notification_URL, method='POST',json=deposit_result)           
@@ -109,19 +127,11 @@ def process_deposit(deposit):
     # - reply from the invocation is not used;
     # continue even if this invocation fails
 
-    # 8. Sent amount deposited
-    # Invoke transaction log microservice
-    print('\n\n-----Publishing the (deposit info) message with routing_key=deposit.info-----') 
-    # transaction_log_URL += "/" + deposit["accID"]
-    # invoke_http(transaction_log_URL, method='POST',json=deposit_result)
-    amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key="deposit.info", 
-            body=message)
-    print("\nDeposit action performed and updated transaction log.\n")
 
     # 9. Return confirmation of deposit
     return {
         "code": 201,
-        "data": {"deposit_result": deposit_result}
+        "data": {"deposit_log": deposit_log}
     }
 
 if __name__ == '__main__':
